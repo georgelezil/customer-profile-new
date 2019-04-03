fe2ff0ebaf0132a1bc48cc02d5f3772a9a9540cc
Lezil George <georgelezil@gmail.com>
2019-04-02 22:30:34 -0500

    Initial commit Spring Boot App and React

---
+++ src/main/java/com/lezil/customerprofilenew/CustomerProfileNewApplication.java
@@ -0,0 +1,13 @@
+package com.lezil.customerprofilenew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerProfileNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerProfileNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lezil/customerprofilenew/CustomerProfileNewApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lezil.customerprofilenew;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomerProfileNewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
